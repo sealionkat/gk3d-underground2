7c1ac94c631a390182bcf54670ab254df6b569bb
sealionkat <sealionkat@gmail.com>
2016-11-13 19:02:24 +0100

    Refactoring

--- project/Camera.h
+++ project/Camera.h
@@ -6,8 +6,8 @@ class Camera
     glm::vec3 Position;
     glm::vec3 Front;
     glm::vec3 Up;
-    GLfloat yaw;
-    GLfloat pitch;
+    GLfloat Yaw;
+    GLfloat Pitch;
 
     glm::mat4 View;
     glm::mat4 Projection;
@@ -17,8 +17,8 @@ class Camera
         Position = glm::vec3(0.0f, 2.0f, 0.0f);
         Front = glm::vec3(0.0f, 0.0f, -1.0f);
         Up = glm::vec3(0.0f, 1.0f, 0.0f);
-        yaw = -90.0f;
-        pitch = 0.0f;
+        Yaw = -90.0f;
+        Pitch = 0.0f;
 
         Projection = glm::perspective(glm::radians(Settings::FOV), (float)width / (float)height, Settings::PerspectiveNear, Settings::PerspectiveFar);
     }
@@ -28,29 +28,29 @@ class Camera
         View = glm::lookAt(Position, Position + Front, Up);
     }
 
-    void DoMovement(glm::vec3 position)
+    void DoMovement(glm::vec3 posOffset)
     {
-        Position += position;
+        Position += posOffset;
     }
 
     void RotateCamera(GLfloat xoffset, GLfloat yoffset)
     {
-        yaw += xoffset;
-        pitch += yoffset;
+        Yaw += xoffset;
+        Pitch += yoffset;
 
-        if (pitch > 89.0f)
+        if (Pitch > 89.0f)
         {
-            pitch = 89.0f;
+            Pitch = 89.0f;
         }
-        if (pitch < -89.0f)
+        if (Pitch < -89.0f)
         {
-            pitch = -89.0f;
+            Pitch = -89.0f;
         }
 
         glm::vec3 front;
-        front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-        front.y = sin(glm::radians(pitch));
-        front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
 
         Front = glm::normalize(front);
     }
